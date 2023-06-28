3673392b3db848661b4224dcfb1eec7175022dc9
danindiana <danindiana@users.noreply.github.com>
2023-06-28 14:54:24 -0500

    Create propellant_charge_calculator.py
    Certainly! Below is the complete code for propellant_charge_calculator.py. This script calculates the propellant charge needed to propel a 240 mm caliber projectile to a desired distance by incorporating a physics model of the mortar firing.
    
    Please keep in mind that this script is a simplified model and assumes certain values (like E_PROP) for the sake of simulation. In real-world applications, more accurate values and additional factors would need to be considered.

---
+++ propellant_charge_calculator.py
@@ -0,0 +1,45 @@
+import math
+
+# Constants
+G = 9.81  # Gravity constant (m/s^2)
+E_PROP = 4500000  # Energy per unit mass of propellant in J/kg (Assumed value)
+
+def calculate_muzzle_velocity(range, angle_deg):
+    """
+    Calculate the muzzle velocity needed to reach a specific range
+    with a launch angle.
+    """
+    angle_rad = math.radians(angle_deg)
+    # Rearranging the range equation to solve for muzzle velocity
+    return math.sqrt((range * G) / (math.sin(2 * angle_rad)))
+
+def calculate_required_energy(mass, velocity):
+    """
+    Calculate the kinetic energy of an object with a given mass and velocity.
+    """
+    return 0.5 * mass * velocity ** 2
+
+def estimate_propellant_charge(energy):
+    """
+    Estimate the mass of the propellant needed to provide a given energy.
+    """
+    return energy / E_PROP
+
+def main():
+    # Given
+    projectile_mass = 134  # kg
+    desired_range = 9000  # m (9 kilometers)
+    launch_angle = 45  # degrees (maximum range for a given velocity is achieved at 45 degrees)
+    
+    # Calculations
+    muzzle_velocity = calculate_muzzle_velocity(desired_range, launch_angle)
+    required_energy = calculate_required_energy(projectile_mass, muzzle_velocity)
+    propellant_charge = estimate_propellant_charge(required_energy)
+    
+    # Output
+    print(f"Muzzle Velocity Required: {muzzle_velocity:.2f} m/s")
+    print(f"Required Energy: {required_energy:.2f} Joules")
+    print(f"Estimated Propellant Charge: {propellant_charge:.2f} kg")
+
+if __name__ == '__main__':
+    main()
